04cf6b13021ec384d97f655fa58fe2dd2e402ebc
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2021-09-26 11:17:21 -0500

    Front-End - Logic without canvas

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,103 @@
+var app = ( function(){
+
+    var _listOfBlueprints;
+    var _inputNombre;
+    var _table = $('#blueprintsTable tbody');
+    var _buttonBlueprints = document.getElementById('buttonBlueprints');
+    var _totalPointsLabel = $('#totalPoints');
+    var _bluePrintsAuthor = $('#bluePrintsAuthor');
+    var _currentBlueprint= $('#currentBluePrint');
+
+    loadEventListeners();
+    function loadEventListeners(){
+        if( !_buttonBlueprints ){
+            console.log('No lo encontre');
+            return;
+        }
+        _buttonBlueprints.addEventListener('click', getBlueprints);
+    }
+
+    function  updateData( totalOfPoints ){
+        _totalPointsLabel.text(`Total Points: ${totalOfPoints}`);
+        _bluePrintsAuthor.text(`${_inputNombre} blueprint's`);
+        _currentBlueprint.text(`Current Blueprint:`);
+    }
+
+    function callB (error , mockDataAuthor) {
+        _listOfBlueprints = mockDataAuthor.map( blueprint => {
+            if( error  ){ return;}
+            const data  = {
+                name:blueprint.name,
+                numberOfPoints: blueprint.points.length
+            };
+            //_totalOfPoints+=data.numberOfPoints;
+            return data;
+        });
+    }
+
+    function getBlueprints(event){
+        readInputData(null);
+    }
+
+
+    function readInputData(bluePrintName){
+        //Limpiamos los datos existentes
+        _listOfBlueprints=[];
+        _inputNombre = $('#inputNombre').val();
+        //Buscamos los blueprints segun el dato ingresado
+        if (bluePrintName === null) apimock.getBlueprintsByAuthor( _inputNombre, callB);
+        else apimock.getBlueprintsByNameAndAuthor(bluePrintName, _inputNombre, callB);
+        _totalOfPoints = _listOfBlueprints.reduce( (total, {numberOfPoints}) => total + numberOfPoints, 0);
+        //Lo pasamos a html
+        bluePrintsHTML(_totalOfPoints);
+    }
+
+    function draw( bluePrintName){
+        //Actualizamos el blueprint seleccionado
+        
+        console.log(`Dibujando ${bluePrintName}`);
+
+        
+    }
+
+    function bluePrintsHTML(totalOfPoints){
+        updateData(totalOfPoints);
+        // Limpiamos el contenido de la tabla HTML
+        _table.empty();
+
+        _listOfBlueprints.map(bluePrint => {
+            const {name, numberOfPoints } = bluePrint;
+            const row = document.createElement('tr');
+            const button = `<button onclick="app.drawBlueprint('${name}')"> Open </button>`;
+            row.innerHTML=`
+                            <td>${name}</td>
+                            <td>${numberOfPoints}</td>
+                            <td>${button}</td>`;
+            //Agregando a la tabla
+            _table.append(row);
+        });
+    }
+
+    function updateName(newName) {
+        $('#inputNombre').val(newName);
+    }
+
+    return {
+            updateAuthorName : newName => {
+                updateName(newName);
+            },
+            setListBlueprintsByAuthor : author => {
+                updateName(author);
+                readInputData(null);
+            },
+            setListBlueprintsByNameAndAuthor : (name,author) => {
+                updateName(author);
+                readInputData(name);
+            },
+            drawBlueprint : (name) =>{
+                draw(name);
+            }
+    }
+})();
+
+
