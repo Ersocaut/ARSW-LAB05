18f43949d1757aefa5d7799279f8e49596f88f70
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2021-09-26 11:09:30 -0500

    Fixing index.html

--- src/test/java/edu/eci/arsw/blueprints/test/services/ApplicationServicesTests.java
+++ src/test/java/edu/eci/arsw/blueprints/test/services/ApplicationServicesTests.java
@@ -2,6 +2,8 @@ package edu.eci.arsw.blueprints.test.services;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import edu.eci.arsw.blueprintsapi.BlueprintsAPIApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest()
+@SpringBootTest( classes = {BlueprintsAPIApplication.class})
 public class ApplicationServicesTests {
     
     @Test
